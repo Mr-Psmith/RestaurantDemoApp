78a644a74ff79acefc3586982a524f1cfdad1ea4
Mr-Psmith <psancthus@gmail.com>
2023-05-02 15:06:12 +0000

    started adding context

---
+++ src/store/cart-context.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CartContext = React.createContext({
+    items: [], /* these arrays wont be used, but gives better autocompletion later*/
+    totalAmount: [],
+    addItem: (item) => {}, /* Recieves the itemtha shuold be added */
+    removeItem: (id) => {} /* recieves an id to identify the item that should be removed from the cart */
+});
