faaae515aa8bb8afb0776909e5cc5bff639e2611
Mr-Psmith <psancthus@gmail.com>
2023-05-09 21:03:04 +0000

    continued context

--- src/components/Layout/header-cart-button.js
+++ src/components/Layout/header-cart-button.js
@@ -7,8 +7,10 @@ import classes from "./header-cart-button.module.css";
 const CartButton = (props) => {
     const cartCtx = useContext(CartContext); /* By using useContext here this comp will be evaluated every time the context changes, like when we update it ofc */
 
-    const numberOfCartItems = cartCtx.items.length;
-
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0); /*Using simple .length would be shit as with calculates with all the arrays length or something like that(there is an item length, and amount problematic(3sushi is 1 item 3 amount)) */
+                                                            /* Thats why we use reduce, as this built in method reduces an array into a simple number */
     return(
         <>
             <button className={classes.button} onClick={props.onClickPr} >
@@ -24,4 +26,4 @@ const CartButton = (props) => {
     );
 };
 
-export default CartButton;
+export default CartButton; 
