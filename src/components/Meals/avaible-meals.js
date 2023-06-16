d5e4c5e79d090c3869acd63f7e15727e35705d91
Mr-Psmith <psancthus@gmail.com>
2023-06-16 16:27:31 +0000

    finished fetch

--- src/components/Meals/avaible-meals.js
+++ src/components/Meals/avaible-meals.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import Card from "../UI/card";
 import MealItem from "./meal-item/meal-item";
@@ -34,9 +34,51 @@ import classes from "./avaible-meals.module.css";
 
 function AvaibleMeals() {
 
-  function getMealsHandler () {
-    fetch("https://database-firebasedemo-default-rtdb.firebaseio.com/meals").than(meals => {
-      const mealsList = meals.map((meal) => ( /* We dont have to store in a const, it just nicer. But we could have just put it where the const is now in raw */
+  // my naiv try(thinking that mx wont give somethin utterly impossible for my skils for excercise):
+  // useEffect(()=>{
+  //   getMealsHandler(); /* this way se trigger the meals when the site is loaded the first time */
+  // }, []); /* here we should put what should next time change when we want to trigger this but cant use the function intself, bec it would do an infinite loop, as functions change at every rerun (or we could use callback)*/
+
+  // async function getMealsHandler () {
+  //   await fetch("https://database-firebasedemo-default-rtdb.firebaseio.com/meals.json").than(meals => {
+  //     const mealsList = meals.map((meal) => ( /* We dont have to store in a const, it just nicer. But we could have just put it where the const is now in raw */
+  //       <MealItem
+  //         key={meal.id}
+  //         id={meal.id}
+  //         name={meal.name}
+  //         description={meal.description}
+  //         price={meal.price}
+  //       />
+  //     ));
+  //   })
+  // };
+
+  const [meals,setMeals] = useState([]);
+
+useEffect(() => {
+  const fetchmeals = async () => {
+    const response = await fetch ("https://database-firebasedemo-default-rtdb.firebaseio.com/meals.json");
+    const responseData = await response.json();
+  
+    /* The firebase stores obj, but we need arrays, so we need to convert them  */
+    const loadedMeals = [];
+
+    for (const key in responseData) {
+      loadedMeals.push({
+        id: key,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: responseData[key].price,
+      })
+    }
+
+    setMeals(loadedMeals);
+  };
+
+  fetchmeals();
+}, []);
+
+const mealsList = meals.map((meal) => ( /* We dont have to store in a const, it just nicer. But we could have just put it where the const is now in raw */
         <MealItem
           key={meal.id}
           id={meal.id}
@@ -44,15 +86,12 @@ function AvaibleMeals() {
           description={meal.description}
           price={meal.price}
         />
-      ));
-    })
-  };
-  
+       ));
 
   return (
     <>
       <section className={classes.meals}>
-        <Card onclickProps={getMealsHandler}>
+        <Card>
           <ul>{mealsList}</ul>
         </Card>
       </section>
